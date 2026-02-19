6a2cc0961e1ae0da50567cbe273ad7469cdd4085
jessicadjedje <Jessicadjedje1@gmail.com>
2026-02-18 22:08:44 -0600

    ading some new files

--- network.tf
+++ network.tf
@@ -92,24 +92,27 @@ resource "aws_eip" "utc_eip_2" {
 resource "aws_eip" "utc_eip_3" {
   domain = "vpc"
 }
-#create NAT gateway
+## NAT Gateways
+# =========================
+
 resource "aws_nat_gateway" "NAT1" {
-  allocation_id = aws_eip.utc-eip.id
+  allocation_id = aws_eip.utc_eip_1.id
   subnet_id     = aws_subnet.public1.id
-  # depends_on = [aws_internet_gateway.example]
+  depends_on    = [aws_internet_gateway.utc-igw]
 }
-#create NAT gateway
+
 resource "aws_nat_gateway" "NAT2" {
-  allocation_id = aws_eip.utc-eip.id
+  allocation_id = aws_eip.utc_eip_2.id
   subnet_id     = aws_subnet.public2.id
-  # depends_on = [aws_internet_gateway.example]
+  depends_on    = [aws_internet_gateway.utc-igw]
 }
-#create NAT gateway
+
 resource "aws_nat_gateway" "NAT3" {
-  allocation_id = aws_eip.utc-eip.id
+  allocation_id = aws_eip.utc_eip_3.id
   subnet_id     = aws_subnet.public3.id
-  # depends_on = [aws_internet_gateway.example]
+  depends_on    = [aws_internet_gateway.utc-igw]
 }
+
 
  #create iGW
 resource "aws_internet_gateway" "utc-igw" {

7bcb5b560697dfcec47ed24f9bd5f9ced410d3dd
jessicadjedje <Jessicadjedje1@gmail.com>
2026-02-18 21:22:59 -0600

    ading some new files

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraformbucketdjedje"
+    bucket = "chatgptbucket-2025-12-27"
     key = "dev/terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
